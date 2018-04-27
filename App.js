d02f79404506fa8ac13fd535b0d1dcaeca380634
sachinkreeti <sgupta@kreeti.com>
2018-04-27 12:09:51 +0530

    added checkbox

--- App.js
+++ App.js
@@ -1,58 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { CheckBox } from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({ checkBox1: 'checked', checkBox2: true });
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  toggleCheckBox = () => this.setState(prevState => ({ checkBox1: !prevState.checkBox1 }));
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <TouchableOpacity >
+        <CheckBox
+          checked={this.state.checkBox1}
+          onPress={this.toggleCheckBox}
+        />
+      </TouchableOpacity>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
